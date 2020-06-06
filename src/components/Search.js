ac840e85925a0a70ba6c2e55051042ad2522e43a
Thanh-Bao <18130017@st.hcmuaf.edu.vn>
2020-06-06 20:25:38 +0700

    thêm nút search

--- src/components/Search.js
+++ src/components/Search.js
@@ -15,12 +15,14 @@ class Search extends Component {
   };
   render() {
     return (
-      <div className=" text-justify text-center">
+      <div className=" text-justify text-center SearchPage ">
         <div id="align-top"></div>
         {this.getUserLocation()}
-        <h2> Tìm kiếm những dự án bất động sản hiện có xung quanh bạn</h2>
-
-        <table className="table table-bordered userLocationTable ">
+        <h2 className="title">
+          <i className="fa fa-street-view"></i> Tìm kiếm những dự án bất động
+          sản hiện có xung quanh bạn
+        </h2>
+        <table className="table table-bordered getUserLocationTable">
           <thead className=" table-success">
             <tr>
               <th scope="col" colSpan="2">
@@ -52,7 +54,64 @@ class Search extends Component {
 
             <tr>
               <th scope="row" colSpan="2">
-                Button
+                {/* bengin button */}
+                <div>
+                  {/* Button trigger modal */}
+                  <button
+                    type="button"
+                    className="btn btn-success"
+                    data-toggle="modal"
+                    data-target="#SearchLocation"
+                  >
+                    <i className="fa fa-search-plus"></i> Search
+                  </button>
+                  {/* Modal */}
+                  <div
+                    className="modal fade"
+                    id="SearchLocation"
+                    tabIndex={-1}
+                    role="dialog"
+                    aria-labelledby="exampleModalCenterTitle"
+                    aria-hidden="true"
+                  >
+                    <div
+                      className="modal-dialog modal-dialog-centered"
+                      role="document"
+                    >
+                      <div className="modal-content">
+                        <div className="modal-header ">
+                          <h5
+                            className="modal-title"
+                          >
+                            <span className="loader loader1"></span> <span className="title" >Đang tìm</span>
+                          </h5>
+                          <button
+                            type="button"
+                            className="close"
+                            data-dismiss="modal"
+                            aria-label="Close"
+                          >
+                            <span aria-hidden="true">
+                              <i className="fa fa-times-circle closeBtn"></i>
+                            </span>
+                          </button>
+                        </div>
+                        <div className="modal-body">sgftggrr adsfrrfgdfasgdsg dffgdfgf dafgdfzg dfgdsfgfsdg sdfsgdfzg fgfrgdfg</div>
+                        <div className="modal-footer">
+                          <button
+                            type="button"
+                            className="btn btn-danger"
+                            data-dismiss="modal"
+                          >
+                            Close
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                {/* end button */}
               </th>
             </tr>
           </tbody>
